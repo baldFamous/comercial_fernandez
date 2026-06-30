0db2bd694835d46284597410b10ad737c8b064d3
bastian rodriguez <bastian3967a@gmail.com>
2026-06-30 00:54:44 -0400

    add service img

--- src/app/servicios/page.tsx
+++ src/app/servicios/page.tsx
@@ -16,7 +16,7 @@ export default function ServiciosPage() {
           <div className="w-full md:w-1/2 h-64 md:h-auto relative overflow-hidden bg-surface-container">
             <div 
               className="absolute inset-0 bg-cover bg-center transition-transform duration-700 group-hover:scale-105" 
-              style={{ backgroundImage: "url('https://lh3.googleusercontent.com/aida-public/AB6AXuBTX_maGZaY9Wo275yVq7Z44-T0WQIEOmuDGjFQh1rrAfwa7bUUWO66D3a07GfKNRCYD9Jjd_OwJgBmU4871O97U6As_xJqwpLYOF_WzciK_l0DBElMHK6wHuU1egwTPX70SytXHIVEOv1G0hn14PTh-rs6xRFP1zjXuBoCYQcFClMuYqt7mCTXCxvhjEj53wvC-3HLpwc8mrCfaQ7R6zS0DHvOkIYm0dT9XkhgNx0RR8fgk30m2mIZ97lWVgxNPYIhbOCSKVp1rpA')" }}
+              style={{ backgroundImage: "url('./img4.webp')" }}
             ></div>
           </div>
           <div className="w-full md:w-1/2 p-8 md:p-12 flex flex-col justify-center">
@@ -52,7 +52,7 @@ export default function ServiciosPage() {
           <div className="h-56 relative overflow-hidden bg-surface-container">
             <div 
               className="absolute inset-0 bg-cover bg-center transition-transform duration-700 group-hover:scale-105" 
-              style={{ backgroundImage: "url('https://lh3.googleusercontent.com/aida-public/AB6AXuBAI-dEBA2_Oju2LpRFtvZC5xm5tmLtabphOZ5Fy4ErbS1TH3NlWbkB3y-7qSj16JK649qZ5ILAe8xjv2lk4bqosuuxDZ8yMW1GtjpVoqj8qDugbqwemn6AEITBwpAFiQmyhGLUFX_UbTuax5p5jmO9J8jnDTQTw-bxEH5-ZthWVyEaXNYH_yF9LZ_V30mWkLufyvjM2yZprWcUxALnhxys22DkskCn0dBpjfNxG5aVU71V9a12RCuR4lQedUX9Vqug97SWMYAQcFQ')" }}
+              style={{ backgroundImage: "url('./img8.webp')" }}
             ></div>
           </div>
           <div className="p-8 flex-grow flex flex-col">
@@ -77,7 +77,7 @@ export default function ServiciosPage() {
           <div className="h-56 relative overflow-hidden bg-surface-container">
             <div 
               className="absolute inset-0 bg-cover bg-center transition-transform duration-700 group-hover:scale-105" 
-              style={{ backgroundImage: "url('https://lh3.googleusercontent.com/aida-public/AB6AXuDuvaL8HJVGSTQOrVcBp6IV6RHRUKvX7fworbVSZetSa4Kc2b5u0bMTTNKLicwoM4YzG-NTxty2ZaVN9YCxaw_Wg8yFLa1LC0_4zqyrgFq-FplNg8tubS69mSVPUJgEYOCoifYfU4DlAIMHdl5CbVCSuQQv-aJc1WhMrmtkccNjM_JvylZt4vdpmOZ1qvQepMwuAAp8hvuFk3iksltPP8g4gA7KfbAwO0_jO_X9VreGNdwSVp1FbX4hJBb1oQrH2NEvL_tjIOVdEdw')" }}
+              style={{ backgroundImage: "url('./img9.webp')" }}
             ></div>
           </div>
           <div className="p-8 flex-grow flex flex-col">
